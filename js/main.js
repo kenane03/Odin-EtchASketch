ee808c919ec86b30348c9107279ac8b4a3ad2ec2
Gwen <98741839+kenane03@users.noreply.github.com>
2023-10-18 11:05:27 +0200

    Add Grid button / Make grid starting value appear

--- js/main.js
+++ js/main.js
@@ -5,14 +5,24 @@ function main() {
     let count = slider.value;
     const containerHeight = container.clientHeight;
     const containerWidth = container.clientWidth;
-    console.log(containerHeight);
-    console.log(containerWidth);
+
+    for (let index = 0; index < slider.value * slider.value; index++) {
+        const boxDiv = document.createElement('div');
+        boxDiv.classList.add('pixel');
+        container.appendChild(boxDiv);
+        count = container.querySelectorAll(".pixel").length;
+    }
+    let height = containerHeight / slider.value;
+    let width = containerWidth / slider.value;
+    console.log(height, width);
+    
+    const boxDivs = document.querySelectorAll(".pixel");
+    boxDivs.forEach(boxDiv => boxDiv.style.height = `${height}px`); 
+    boxDivs.forEach(boxDiv => boxDiv.style.width = `${width}px`); 
 
 
     slider.oninput = () => {
         value.textContent = `${slider.value}x${slider.value}`;
-
-        
 
     for (let index = 0; index < slider.value * slider.value; index++) {
         const boxDiv = document.createElement('div');
